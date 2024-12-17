460108fd8af1ebcbab918121a2e14e01c7565805
Miftah28 <miftahussurur280602@gmail.com>
2024-12-18 05:01:53 +0700

    ubah icon

--- resources/views/layouts/component/sidebar.blade.php
+++ resources/views/layouts/component/sidebar.blade.php
@@ -14,19 +14,19 @@
         <li class="nav-item">
             <a class="nav-link {{ request()->is('penjual/jenis-barang') ? 'active' : 'collapsed' }}"
                 href="{{route('Penjual.jenis-barang.index')}}">
-                <i class="bi bi-person-exclamation"></i><span>Jenis Barang</span>
+                <i class="bi bi-box"></i><span>Jenis Barang</span>
             </a>
         </li><!-- End konfirmasi Akun -->
         <li class="nav-item">
             <a class="nav-link {{ request()->is('penjual/barang') ? 'active' : 'collapsed' }}"
                 href="{{route('Penjual.barang.index')}}">
-                <i class="bi bi-gear-fill"></i><span>Barang</span>
+                <i class="bi bi-box-seam"></i><span>Barang</span>
             </a>
         </li><!-- Setting -->
         <li class="nav-item">
             <a class="nav-link {{ request()->is('penjual/barang-terjual') ? 'active' : 'collapsed' }}"
                 href="{{route('Penjual.barang-terjual.index')}}">
-                <i class="bi bi-envelope-paper"></i><span>Barang Terjual</span>
+                <i class="bi bi-receipt"></i><span>Barang Terjual</span>
             </a>
         </li><!-- Setting -->
 
